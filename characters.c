0ffdf1b4d7ffcc8192ab462940144c13e605959b
Mr-Lizzard <k3vincostas@gmail.com>
2026-06-29 22:02:24 -0300

    fixed heal infinite loop

--- characters.c
+++ characters.c
@@ -1,32 +1,27 @@
 #include "characters.h"
 
 void warriorAttack(Character* character, Character* target) {
-    if (character->classes == 1) { // Assuming class 1 is Warrior
         int damage = character->damage*2;
         takeDamage(target, damage);
         printf("%s attacks %s and deals %d damage!\n", character->name, target->name, damage);
-    }
 }
 
 void mageAttack(Character* character, Character* target1, Character* target2, Character* target3) {
-    if (character->classes == 2) { // Assuming class 2 is Mage
         int damage = character->damage;
         takeDamage(target1, damage);
         takeDamage(target2, damage);
         takeDamage(target3, damage);
-        printf("%s casts a spell on the enemies dealing %d damage each!\n", character->name, target1->name, target2->name, target3->name, damage);
-    }
+        printf("%s casts a spell on the enemies dealing %d damage each!\n", character->name,   damage);
 }
 
-void heal(Character* character, Character* target) {
-  int i;
-  if(target->currentHp < target->maxHp) {
+void heal(Character* character) {
+
+  int i = 0;
+  if(character->currentHp < character->maxHp) {
     do
     {
-      target->currentHp++;
+      character->currentHp++;
       i++;
-    } while (i == 2 || target->currentHp == target->maxHp);
-    
+    } while (i != 2 && character->currentHp == character->maxHp);
   }
-
 }
--- characters.h
+++ characters.h
@@ -9,7 +9,7 @@ typedef struct {
 
 void warriorAttack(Character* character, Character* target);
 void mageAttack(Character* character, Character* target1, Character* target2, Character* target3);
-void heal(Character* character, int amount);
+void heal(Character* character);
 void takeDamage(Character* character, int amount);
 void verifyClass(Character* character);
 void rollDex(Character* character);
